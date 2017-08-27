f7184210db35247f975a91689387642f657f89ae
Anton Ukhanev <xedin.unknown@gmail.com>
2017-08-27 15:56:28 +0200

    Added `CouldNotReadExceptionInterface`

---
+++ src/Exception/CouldNotReadExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Io\Resource\Exception;
+
+/**
+ * An exception which occurs when a resource cannot be read from.
+ *
+ * @since [*next-version*]
+ */
+interface CouldNotReadExceptionInterface extends ResourceExceptionInterface
+{
+}
